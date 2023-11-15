8f054c72656166b8e682d835be6bea4e1d13248a
OhMinSeop <msoh@2online.io>
2023-11-15 19:00:12 +0900

    add:코드 수정

--- src/defer/getNeusralCollection.ts
+++ src/defer/getNeusralCollection.ts
@@ -1,6 +1,10 @@
 // the `defer()` helper will be used to define a background function
 import { defer } from "@defer/client";
-import puppeteer from "puppeteer";
+import puppeteer, {
+  type Browser,
+  type Page,
+  type ElementHandle,
+} from "puppeteer";
 import hash from "stable-hash";
 
 type Item = {
@@ -13,6 +17,205 @@ type Item = {
   date: string | undefined;
   description: string | undefined;
 };
+
+class Nouvelles {
+  private _URL = "https://www.neusral.com/briefing_subscriptions";
+
+  private _browser: Browser | undefined;
+
+  private _page: Page | undefined;
+
+  private _$targetContainer: ElementHandle<Element>[] | undefined;
+
+  constructor() {
+    this.init();
+  }
+
+  async init() {
+    // make sure to pass the `--no-sandbox` option
+    this._browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+
+    try {
+      this._page = await this._browser.newPage();
+
+      // pass the following `waitUntil` option to avoid
+      // unnecessary blocking when loading a page
+      await this._page.goto(this._URL, { waitUntil: "networkidle0" });
+
+      // ".slick-list draggable" is the selector of the element
+      const $ele1 = await this._findTargetContainer();
+
+      const items = await this._findNews($ele1);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await this.dispose();
+    }
+  }
+
+  private async _findTargetContainer() {
+    if (!this._page) {
+      throw new Error("No page found");
+    }
+
+    const $ele = await this._page.$$(".slick-list.draggable");
+    if (!$ele) {
+      throw new Error("No container found");
+    }
+
+    return $ele;
+  }
+
+  private async _findByParentCategory($ele: ElementHandle<Element>) {
+    const text = await $ele.$eval(
+      ".report-header .left .title",
+      (el) => el.textContent
+    );
+
+    if (!text) {
+      return undefined;
+    }
+
+    return text.trim();
+  }
+
+  private async _findByChildCategory($ele: ElementHandle<Element>) {
+    const text = await $ele.$eval(
+      ".tab-name > .tab_input_box",
+      (el) => el.textContent
+    );
+
+    if (!text) {
+      return undefined;
+    }
+
+    return text.trim();
+  }
+
+  private async _findByReporter($ele: ElementHandle<Element>) {
+    const text = await $ele.$eval(
+      ".report-header .left .reporter",
+      (el) => el.textContent
+    );
+
+    if (!text) {
+      return undefined;
+    }
+
+    return text.trim();
+  }
+
+  private async _findByEachTab($ele: ElementHandle<Element>) {
+    const $tabs = await $ele.$$(".each-tab");
+    if (!$tabs) {
+      return [];
+    }
+    return $tabs;
+  }
+
+  private async _findByNews($ele: ElementHandle<Element>) {
+    const $tabs = await $ele.$$("a[class='news-url']");
+    if (!$tabs) {
+      return [];
+    }
+    return $tabs;
+  }
+
+  private async _findByTitle($ele: ElementHandle<Element>) {
+    const text = await $ele.$eval(".news-title > span", (el) => el.textContent);
+
+    if (!text) {
+      return undefined;
+    }
+
+    return text.trim();
+  }
+
+  private async _findByLink($ele: ElementHandle<Element>) {
+    const text = await $ele.$eval(".news-list", (e) => {
+      return e.parentElement?.getAttribute("href");
+    });
+
+    if (!text) {
+      return undefined;
+    }
+
+    return text.trim();
+  }
+
+  private async _findByDate($ele: ElementHandle<Element>) {
+    const text = await $ele.$eval(
+      ".news-date span[class='news-date-text']",
+      (el) => el.textContent
+    );
+
+    if (!text) {
+      return undefined;
+    }
+
+    return text.trim();
+  }
+
+  private async _findByDescription($ele: ElementHandle<Element>) {
+    const text = await $ele.$eval(".news-description", (el) => el.textContent);
+
+    if (!text) {
+      return undefined;
+    }
+
+    return text.trim();
+  }
+
+  private async _findNews($eles: ElementHandle<Element>[]) {
+    const items: Map<string, Item> = new Map();
+
+    for (const $item of $eles) {
+      const parentCategory = await this._findByParentCategory($item);
+      const reporter = await this._findByReporter($item);
+      const $tabs = await this._findByEachTab($item);
+
+      for (const $tab of $tabs) {
+        const childCategory = await this._findByChildCategory($tab);
+        const $news = await this._findByNews($tab);
+
+        for (const $new of $news) {
+          const title = await this._findByTitle($new);
+          const link = await this._findByLink($new);
+          const date = await this._findByDate($new);
+          const description = await this._findByDescription($new);
+
+          const input = {
+            parentCategory,
+            childCategory,
+            reporter,
+            title,
+            link,
+            date,
+            description,
+          };
+
+          const id = hash(input);
+          const nextInput = {
+            id,
+            ...input,
+          };
+
+          if (!items.has(id)) {
+            items.set(id, nextInput);
+          }
+        }
+      }
+    }
+
+    return Array.from(items.values());
+  }
+
+  async dispose() {
+    if (this._browser) {
+      await this._browser.close();
+    }
+  }
+}
 
 // a background function must be `async`
 async function getNeusralCollection() {
