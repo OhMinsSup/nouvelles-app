1e825fe336f8656273fc4d99e1a81e510923f997
OhMinSeop <mins5190@gmail.com>
2023-12-16 01:36:48 +0900

    add: 수정

--- apps/nouvelles/env.mjs
+++ apps/nouvelles/env.mjs
@@ -1,10 +1,10 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    NODE_ENV: z.enum(["development", "test", "production"]),
-    DEPLOY_GROUP: z.enum(["development", "local", "production"]),
+    NODE_ENV: z.enum(['development', 'test', 'production']),
+    DEPLOY_GROUP: z.enum(['development', 'local', 'production']),
     KAKAO_CLIENT_ID: z.string().min(1),
     NEXTAUTH_SECRET: z.string().min(1),
     KAKAO_CLIENT_SECRET: z.string().min(1),
