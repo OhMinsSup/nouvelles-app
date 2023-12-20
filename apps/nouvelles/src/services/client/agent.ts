4f8e480bb435fc6f091871f6c4345e88d822e7a4
OhMinSeop <msoh@2online.io>
2023-12-20 18:26:58 +0900

    add: 수정

--- apps/nouvelles/src/services/client/agent.ts
+++ apps/nouvelles/src/services/client/agent.ts
@@ -1,15 +1,7 @@
 import { NouvellesAgent } from '@nouvelles/api';
 import { env } from 'env.mjs';
-import { parseUrl } from '~/utils/parseUrl';
-
-let service: string;
-if (env.NEXT_PUBLIC_VERCEL_URL) {
-  service = parseUrl(env.NEXT_PUBLIC_VERCEL_URL).origin;
-} else {
-  service = env.NEXT_PUBLIC_SITE_URL;
-}
 
 export const agent = new NouvellesAgent({
-  service,
+  service: env.NEXT_PUBLIC_SITE_URL,
   prefix: '/api',
 });
