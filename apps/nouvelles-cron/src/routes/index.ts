9c48ab188de6ac86ffc809f8e40b5f5f9f2ce076
OhMinSeop <msoh@2online.io>
2023-12-20 16:48:15 +0900

    add: server 헬스체크 API 추가 및 fly.io deploy 준비

--- apps/nouvelles-cron/src/routes/index.ts
+++ apps/nouvelles-cron/src/routes/index.ts
@@ -1,15 +1,28 @@
-import dayjs from 'dayjs';
+import { container } from 'tsyringe';
 import type { FastifyPluginCallback } from 'fastify';
+import { CommonService } from '~/services/common.service';
 import items from './items';
 
 const api: FastifyPluginCallback = (fastify, opts, done) => {
+  const commonService = container.resolve(CommonService);
+
   fastify.register(items, { prefix: '/items' });
 
   fastify.get('/ping', (request, reply) => {
-    const now = new Date();
-    const serverTime = dayjs(now).format('yyyy-MM-DD HH:mm:ss');
+    const serverTime = commonService.getServerTime();
     reply.send({ serverTime });
   });
+
+  fastify.get('/healthcheck', async (request, reply) => {
+    try {
+      await commonService.healthcheck();
+      reply.send({ ok: true });
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ ok: false });
+    }
+  });
+
   done();
 };
 
