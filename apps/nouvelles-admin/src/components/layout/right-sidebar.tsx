ee69520ac89992076aff10d86184ac49d898ad52
OhMinSeop <mins5190@gmail.com>
2024-01-10 21:05:50 +0900

    add: 사용안하는 컴포넌트 제거

--- apps/nouvelles-admin/src/components/layout/right-sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function RightSidebar() {
-  return (
-    <div className="w-80 max-h-full absolute right-desktop-sidebar">
-      <div className="py-5" />
-    </div>
-  );
-}
